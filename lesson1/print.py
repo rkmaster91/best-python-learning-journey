87891a1559568cced9730f03d5ae4fd8abe2ab10
Rkmaster91 <rkmaster91@gmail.com>
2025-12-31 19:15:29 +0530

    1st commit

---
+++ lesson1/print.py
@@ -0,0 +1,7 @@
+print("killer RK")
+print("my age is 13")
+print("killer RK", "my age is 13" )
+print(67)
+print(67+67)
+
+
---
+++ lesson1/variables.py
@@ -0,0 +1,15 @@
+name = "Rehan Khurana"
+age = 13
+price_of_my_laptop = 57000.50
+
+print(name)
+print(price_of_my_laptop)
+
+print("my name is : ", name)
+print("my age is : ", age)
+print("the price of my laptop is : ", price_of_my_laptop)
+
+#for eg i will perform data assignment 
+age2 = age
+
+#to print the type of a variable
