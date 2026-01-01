c94ca8679ee16dd572354719dbc77a6a0e811905
rkmaster91 <rkmaster91@gmail.com>
2026-01-01 14:15:34 +0530

    5th commit

---
+++ lesson1(basics)/operator.py
@@ -0,0 +1,11 @@
+# arithematic operators ( +,-,*/,%,**)
+a = 5
+b = 67
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # print remainder ie modulus
+print(a//b) # print the quotient without the decimal
+print(a**b)
