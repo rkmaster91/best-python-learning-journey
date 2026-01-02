655b56da3d5277b133c028e806d270e15b5bd2f7
rkmaster91 <rkmaster91@gmail.com>
2026-01-02 12:18:12 +0530

    6th commit

--- lesson1(basics)/operator.py
+++ lesson1(basics)/operator.py
@@ -7,5 +7,5 @@ print(a-b)
 print(a*b)
 print(a/b)
 print(a%b) # print remainder ie modulus
-print(a//b) # print the quotient without the decimal
+print(a//b) # print the quotient without the decimal   
 print(a**b)
